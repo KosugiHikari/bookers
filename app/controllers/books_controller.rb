bdd7d25304bc70a9eb44942e1cae6c0e0d99a439
EC2 Default User <ec2-user@ip-172-31-38-252.ap-northeast-1.compute.internal>
2023-02-04 14:28:08 +0000

    [Update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,4 +4,22 @@ class BooksController < ApplicationController
     
   end
   
+
+  
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/show'
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   
   root to: 'homes#top'
   
+  get 'books' => 'books#home'
+  
+  post 'books' => 'books#create'
+  
 end
